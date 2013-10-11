bb55cebe053902b54b9c59af9c7faf9a3478caff
Christos Kanellopoulos <skanct@gmail.com>
2013-10-11 13:38:53 +0300

    Added Initial Test
    - Test the XML generated by the createXMLResponse method

---
+++ service_availability_in_profile_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/xml"
+	"github.com/makistsan/go-lru-cache"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCreateXMLResponse(t *testing.T) {
+	var v []byte
+	var tl Timeline
+	expected_response := ` <root>
+   <Profile name="ROC_CRITICAL" namespace="ch.cern.sam" defined_by_vo_name="ops">
+     <Service hostname="lcgbdii.phy.bris.ac.uk" type="CREAM-CE" flavor="CREAM-CE">
+       <Availability timestamp="2013-08-01T00:00:00Z" availability="1" reliability="1" maintenance="-1"></Availability>
+       <Availability timestamp="2013-08-01T01:00:00Z" availability="1" reliability="1" maintenance="-1"></Availability>
+     </Service>
+   </Profile>
+ </root>`
+	tl.Date = 20130801
+	tl.Host = "lcgbdii.phy.bris.ac.uk"
+	tl.Namespace = "ch.cern.sam"
+	tl.Profile = "ROC_CRITICAL"
+	tl.ServiceFlavor = "CREAM-CE"
+	tl.Timeline = "[1:1:-1, 1:1:-1]\n"
+	tl.VO = "ops"
+
+	results := []Timeline{tl}
+	v, _ = createXMLResponse(results)
+	if string(v) != expected_response {
+		t.Error("XML response is not correct", string(v))
+	}
+}
