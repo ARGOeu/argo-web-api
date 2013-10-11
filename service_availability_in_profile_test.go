c458eab54c6806a860bf58ebb62d73ea95b4d43d
Christos Kanellopoulos <skanct@gmail.com>
2013-10-11 17:12:09 +0300

    Added test case for the API
    - Call the service_availability_in_profile API to get the
      availabilities using two service hostnames in the query

--- service_availability_in_profile_test.go
+++ service_availability_in_profile_test.go
@@ -67,3 +67,16 @@ func TestServiceAvailabilityInProfileQueryWithOneServiceHostname(t *testing.T) {
 		t.Error("Expected to find 1 services, but instead found", len(xmlStruct.Profile[0].Service))
 	}
 }
+
+func TestServiceAvailabilityInProfileQueryWithTwoServiceHostnames(t *testing.T) {
+	httpcache = cache.NewLRUCache(uint64(700000000))
+	xmlStruct := Root{}
+	request, _ := http.NewRequest("GET", "/api/v1/service_availability_in_profile?vo_name=ops&group_type=Site&start_time=2013-08-01T23:00:00Z&end_time=2013-08-01T23:59:00Z&type=HOURLY&output=XML&profile_name=ROC_CRITICAL&service_hostname=sbdii.afroditi.hellasgrid.gr&service_hostname=mon.kallisto.hellasgrid.gr", nil)
+	response := httptest.NewRecorder()
+	err := xml.Unmarshal([]byte(ServiceAvailabilityInProfile(response, request)), &xmlStruct)
+	if err != nil {
+		t.Error("Error parsing XML file: %v", err)
+	} else if len(xmlStruct.Profile[0].Service) != 2 {
+		t.Error("Expected to find 2 services, but instead found", len(xmlStruct.Profile[0].Service))
+	}
+}
