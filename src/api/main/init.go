ab0b39b52a758b6ed650b30320a2509ba1c907d7
Nikos Triantafillidis <ntrianta@lab.grid.auth.gr>
2014-02-25 16:47:37 +0200

    added separate init file

---
+++ src/api/main/init.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/makistsan/go-lru-cache"
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+	"runtime/pprof"
+)
+
+var httpcache *cache.LRUCache
+
+type mystring string
+
+func (s mystring) Size() int {
+	return len(s)
+}
+
+// Load the configurations that we have set through flags and through the configuration file
+var cfg = LoadConfiguration()
+
+func init() {
+
+	//Create a recover function to log the case of a failure
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("work failed:", err)
+		}
+	}()
+
+	//Initialize the cache
+	httpcache = cache.NewLRUCache(uint64(cfg.Server.Lrucache))
+
+	//Set GOMAXPROCS
+	runtime.GOMAXPROCS(cfg.Server.Maxprocs)
+
+	//Start the profiler if the flag flProfile is set to a filename, where profile data will be writter
+	if *flProfile != "" {
+		f, err := os.Create(*flProfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	//Catch an terminate signal and write all profiling data before exiting
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			// sig is a ^C, handle it
+			if *flProfile != "" {
+				pprof.StopCPUProfile()
+			}
+			log.Printf("captured %v, stopping profiler and exiting..", sig)
+			os.Exit(1)
+		}
+	}()
+}
