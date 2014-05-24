4d38d7f3815e06f8e0b204f89cc58264e7de37ea
Christos Kanellopoulos <skanct@gmail.com>
2014-05-24 20:35:09 +0300

    Updates copyright

--- app/recomputations/recomputationsController.go
+++ app/recomputations/recomputationsController.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 GRNET S.A., SRCE, IN2P3 CNRS Computing Centre
+ * Copyright (c) 2014 GRNET S.A.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the
