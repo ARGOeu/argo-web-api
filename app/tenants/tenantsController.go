c5b70fcef76091cdceed0098c6bd2bb01559f065
Konstantinos Kagkelidis <kaggis@gmail.com>
2015-06-09 15:00:29 +0300

    ARGO-119 - Refactor conditionals in create function

--- app/tenants/tenantsController.go
+++ app/tenants/tenantsController.go
@@ -27,80 +27,64 @@ func Create(r *http.Request, cfg config.Config) (int, http.Header, []byte, error
 
 	message := ""
 
-	//Authentication procedure
-	if authentication.Authenticate(r.Header, cfg) {
+	// if authentication procedure fails then
+	// return unauthorized
+	if authentication.Authenticate(r.Header, cfg) == false {
 
-		session, err := mongo.OpenSession(cfg.MongoDB)
+		output = []byte(http.StatusText(http.StatusUnauthorized))
+		//If wrong api key is passed we return UNAUTHORIZED http status
+		code = http.StatusUnauthorized
+		h.Set("Content-Type", fmt.Sprintf("%s; charset=%s", contentType, charset))
+		return code, h, output, err
+	}
 
+	//Reading the json input
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	input := Tenant{}
+	results := []Tenant{}
+	//Unmarshalling the json input into byte form
+	err = json.Unmarshal(reqBody, &input)
+
+	if err != nil {
 		if err != nil {
-			code = http.StatusInternalServerError
-			return code, h, output, err
-		}
-
-		//Reading the json input
-		reqBody, err := ioutil.ReadAll(r.Body)
-
-		input := Tenant{}
-		results := []Tenant{}
-		//Unmarshalling the json input into byte form
-		err = json.Unmarshal(reqBody, &input)
-
-		if err != nil {
-			if err != nil {
-				message = "Malformated json input data" // User provided malformed json input data
-				output, err := messageXML(message)
-
-				if err != nil {
-					code = http.StatusInternalServerError
-					return code, h, output, err
-				}
-
-				code = http.StatusBadRequest
-				h.Set("Content-Type", fmt.Sprintf("%s; charset=%s", contentType, charset))
-				return code, h, output, err
-			}
-		}
-
-		//Making sure that no profile with the requested name and namespace combination already exists in the DB
-
-		query := searchName(input.Name)
-		err = mongo.Find(session, cfg.MongoDB.Db, "tenants", query, "name", &results)
-
-		if err != nil {
-			code = http.StatusInternalServerError
-			return code, h, output, err
-		}
-
-		// If no name is found then add the new tenant
-		if len(results) <= 0 {
-			//If name-namespace combination is unique we insert the new record into mongo
-			query := createTenant(input)
-			err = mongo.Insert(session, cfg.MongoDB.Db, "tenants", query)
+			message = "Malformated json input data" // User provided malformed json input data
+			output, err := messageXML(message)
 
 			if err != nil {
 				code = http.StatusInternalServerError
 				return code, h, output, err
 			}
 
-			mongo.CloseSession(session)
-
-			//Providing with the appropriate user response
-			message = "Tenant inforsmation successfully added"
-			output, err := messageXML(message) //Render the response into XML
-
-			if err != nil {
-				code = http.StatusInternalServerError
-				return code, h, output, err
-			}
-
+			code = http.StatusBadRequest
 			h.Set("Content-Type", fmt.Sprintf("%s; charset=%s", contentType, charset))
 			return code, h, output, err
-
 		}
+	}
 
+	// Try to open the mongo session
+	session, err := mongo.OpenSession(cfg.MongoDB)
+	defer session.Close()
+
+	if err != nil {
+		code = http.StatusInternalServerError
+		return code, h, output, err
+	}
+
+	//Making sure that no profile with the requested name and namespace combination already exists in the DB
+
+	query := searchName(input.Name)
+	err = mongo.Find(session, cfg.MongoDB.Db, "tenants", query, "name", &results)
+
+	if err != nil {
+		code = http.StatusInternalServerError
+		return code, h, output, err
+	}
+
+	if len(results) > 0 {
 		// Name was found so print the error message
 		message = "Tenant with the same name already exists"
-		output, err := messageXML(message) //Render the response into XML
+		output, err = messageXML(message) //Render the response into XML
 
 		if err != nil {
 			code = http.StatusInternalServerError
@@ -112,9 +96,25 @@ func Create(r *http.Request, cfg config.Config) (int, http.Header, []byte, error
 		return code, h, output, err
 
 	}
-	// Unauthorized user
-	output = []byte(http.StatusText(http.StatusUnauthorized))
-	code = http.StatusUnauthorized //If wrong api key is passed we return UNAUTHORIZED http status
+
+	//If name-namespace combination is unique we insert the new record into mongo
+	query = createTenant(input)
+	err = mongo.Insert(session, cfg.MongoDB.Db, "tenants", query)
+
+	if err != nil {
+		code = http.StatusInternalServerError
+		return code, h, output, err
+	}
+
+	//Providing with the appropriate user response
+	message = "Tenant information successfully added"
+	output, err = messageXML(message) //Render the response into XML
+
+	if err != nil {
+		code = http.StatusInternalServerError
+		return code, h, output, err
+	}
+
 	h.Set("Content-Type", fmt.Sprintf("%s; charset=%s", contentType, charset))
 	return code, h, output, err
 
