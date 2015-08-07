cab876b9660d8653c75cd9c910eb267bcba64c65
Konstantinos Kagkelidis <kaggis@gmail.com>
2015-08-07 13:55:29 +0300

    ARGO-182 add initial mvc structure

---
+++ app/statusEndpointGroups/controller.go
@@ -0,0 +1,101 @@
+package statusEndpointGroups
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/ARGOeu/argo-web-api/utils/authentication"
+	"github.com/ARGOeu/argo-web-api/utils/config"
+	"github.com/ARGOeu/argo-web-api/utils/mongo"
+	"github.com/gorilla/mux"
+	"labix.org/v2/mgo/bson"
+)
+
+// ListMetricTimelines returns a list of metric timelines
+func ListMetricTimelines(r *http.Request, cfg config.Config) (int, http.Header, []byte, error) {
+
+	//STANDARD DECLARATIONS START
+
+	code := http.StatusOK
+	h := http.Header{}
+	output := []byte("List Metric Timelines")
+	err := error(nil)
+	contentType := "text/xml"
+	charset := "utf-8"
+
+	//STANDARD DECLARATIONS END
+
+	// Parse the request into the input
+	urlValues := r.URL.Query()
+	vars := mux.Vars(r)
+
+	input := InputParams{
+		urlValues.Get("start_time"),
+		urlValues.Get("end_time"),
+		vars["report_name"],
+		vars["group_type"],
+		vars["group_name"],
+	}
+
+	// Call authenticateTenant to check the api key and retrieve
+	// the correct tenant db conf
+	tenantDbConfig, err := authentication.AuthenticateTenant(r.Header, cfg)
+
+	if err != nil {
+		output = []byte(http.StatusText(http.StatusUnauthorized))
+		code = http.StatusUnauthorized //If wrong api key is passed we return UNAUTHORIZED http status
+		h.Set("Content-Type", fmt.Sprintf("%s; charset=%s", contentType, charset))
+		return code, h, output, err
+	}
+
+	// Mongo Session
+	results := []DataOutput{}
+
+	session, err := mongo.OpenSession(tenantDbConfig)
+	defer mongo.CloseSession(session)
+
+	metricCollection := session.DB(tenantDbConfig.Db).C("status_services")
+
+	fmt.Println("this is a call to print services")
+	fmt.Println(input)
+
+	// Query the detailed metric results
+	err = metricCollection.Find(prepareQuery(input)).All(&results)
+	if err != nil {
+		code = http.StatusInternalServerError
+		return code, h, output, err
+	}
+
+	fmt.Println(results)
+
+	output, err = createView(results, input) //Render the results into XML format
+
+	h.Set("Content-Type", fmt.Sprintf("%s; charset=%s", contentType, charset))
+	return code, h, output, err
+}
+
+func prepareQuery(input InputParams) bson.M {
+
+	//Time Related
+	const zuluForm = "2006-01-02T15:04:05Z"
+	const ymdForm = "20060102"
+
+	ts, _ := time.Parse(zuluForm, input.startTime)
+	te, _ := time.Parse(zuluForm, input.endTime)
+	tsYMD, _ := strconv.Atoi(ts.Format(ymdForm))
+	teYMD, _ := strconv.Atoi(te.Format(ymdForm))
+
+	// prepare the match filter
+	filter := bson.M{
+		"date_integer": bson.M{"$gte": tsYMD, "$lte": teYMD},
+		"report":       input.report,
+	}
+
+	if len(input.service) > 0 {
+		filter["service"] = input.service
+	}
+
+	return filter
+}
---
+++ app/statusEndpointGroups/view.go
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2014 GRNET S.A., SRCE, IN2P3 CNRS Computing Centre
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ *
+ * The views and conclusions contained in the software and
+ * documentation are those of the authors and should not be
+ * interpreted as representing official policies, either expressed
+ * or implied, of either GRNET S.A., SRCE or IN2P3 CNRS Computing
+ * Centre
+ *
+ * The work represented by this source file is partially funded by
+ * the EGI-InSPIRE project through the European Commission's 7th
+ * Framework Programme (contract # INFSO-RI-261323)
+ */
+
+package statusEndpointGroups
+
+import (
+	"encoding/xml"
+)
+
+func createView(results []DataOutput, input InputParams) ([]byte, error) {
+
+	docRoot := &rootXML{}
+
+	if len(results) == 0 {
+		output, err := xml.MarshalIndent(docRoot, " ", "  ")
+		return output, err
+	}
+
+	prevEndpointGroup := ""
+	prevService := ""
+
+	var ppEndpointGroup *endpointGroupXML
+	var ppService *serviceXML
+
+	for _, row := range results {
+
+		if row.EndpointGroup != prevEndpointGroup && row.EndpointGroup != "" {
+			endpointGroup := &endpointGroupXML{}
+			endpointGroup.Name = row.EndpointGroup
+			endpointGroup.GroupType = input.groupType
+			docRoot.EndpointGroups = append(docRoot.EndpointGroups, endpointGroup)
+			prevEndpointGroup = row.EndpointGroup
+			ppEndpointGroup = endpointGroup
+		}
+
+		if row.Service != prevService && row.Service != "" {
+			service := &serviceXML{}
+			service.Name = row.Service
+			service.GroupType = "service"
+			ppEndpointGroup.Services = append(ppEndpointGroup.Services, service)
+
+			prevService = row.Service
+			ppService = service
+		}
+
+		status := &statusXML{}
+		status.Timestamp = row.Timestamp
+		status.Value = row.Status
+		ppService.Statuses = append(ppService.Statuses, status)
+
+	}
+
+	output, err := xml.MarshalIndent(docRoot, " ", "  ")
+	return output, err
+
+}
+
+func messageXML(answer string) ([]byte, error) {
+	docRoot := &message{}
+	docRoot.Message = answer
+	output, err := xml.MarshalIndent(docRoot, " ", "  ")
+	return output, err
+}
