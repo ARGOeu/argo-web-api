2f58b0c289c41f024b90fcd50a2dc92762c6d047
Konstantinos Kagkelidis <kaggis@gmail.com>
2019-09-10 15:43:15 +0300

    ARGO-1964 Return all daily metric data for specific host and date

--- app/metricResult/controller.go
+++ app/metricResult/controller.go
@@ -35,6 +35,55 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+// GetMultipleMetricResults returns the detailed message from a probe
+func GetMultipleMetricResults(r *http.Request, cfg config.Config) (int, http.Header, []byte, error) {
+
+	//STANDARD DECLARATIONS START
+	code := http.StatusOK
+	h := http.Header{}
+	output := []byte("")
+	err := error(nil)
+	charset := "utf-8"
+	//STANDARD DECLARATIONS END
+
+	// Set Content-Type response Header value
+	contentType := r.Header.Get("Accept")
+	h.Set("Content-Type", fmt.Sprintf("%s; charset=%s", contentType, charset))
+
+	// Grab Tenant DB configuration from context
+	tenantDbConfig := context.Get(r, "tenant_conf").(config.MongoConfig)
+
+	vars := mux.Vars(r)
+	urlValues := r.URL.Query()
+
+	input := metricResultQuery{
+		EndpointName: vars["endpoint_name"],
+		ExecTime:     urlValues.Get("exec_time"),
+	}
+
+	session, err := mongo.OpenSession(tenantDbConfig)
+	defer mongo.CloseSession(session)
+
+	if err != nil {
+		code = http.StatusInternalServerError
+		return code, h, output, err
+	}
+
+	results := []metricResultOutput{}
+
+	// Query the detailed metric results
+	err = mongo.Pipe(session, tenantDbConfig.Db, "status_metrics", prepMultipleQuery(input), &results)
+
+	output, err = createMultipleMetricResultsView(results, contentType)
+
+	if err != nil {
+		code = http.StatusInternalServerError
+		return code, h, output, err
+	}
+
+	return code, h, output, err
+}
+
 // GetMetricResult returns the detailed message from a probe
 func GetMetricResult(r *http.Request, cfg config.Config) (int, http.Header, []byte, error) {
 
@@ -105,6 +154,55 @@ func prepQuery(input metricResultQuery) bson.M {
 		"host":         input.EndpointName,
 		"metric":       input.MetricName,
 		"time_integer": tsInt,
+	}
+
+	return query
+
+}
+
+func prepMultipleQuery(input metricResultQuery) []bson.M {
+
+	//Time Related
+	const zuluForm = "2006-01-02T15:04:05Z"
+	const ymdForm = "20060102"
+
+	ts, _ := time.Parse(zuluForm, input.ExecTime)
+	tsYMD, _ := strconv.Atoi(ts.Format(ymdForm))
+
+	filter := bson.M{
+		"date_integer": tsYMD,
+		"host":         input.EndpointName,
+	}
+
+	query := []bson.M{
+		{"$match": filter},
+		{"$group": bson.M{
+			"_id": bson.M{
+				"host":      "$host",
+				"service":   "$service",
+				"metric":    "$metric",
+				"timestamp": "$timestamp",
+				"message":   "$message",
+				"summary":   "$summary",
+				"status":    "$status"},
+		},
+		},
+		{"$project": bson.M{
+			"_id":       0,
+			"host":      "$_id.host",
+			"metric":    "$_id.metric",
+			"service":   "$_id.service",
+			"timestamp": "$_id.timestamp",
+			"status":    "$_id.status",
+			"summary":   "$_id.summary",
+			"message":   "$_id.message"},
+		},
+		{"$sort": bson.D{
+			{"service", 1},
+			{"metric", 1},
+			{"timestamp", 1},
+		},
+		},
 	}
 
 	return query
