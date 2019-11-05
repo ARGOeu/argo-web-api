76b630dbdbf5617fae122cae94ee49cfeef7cd28
Konstantinos Kagkelidis <kaggis@gmail.com>
2019-11-05 16:05:26 +0200

    ARGO-2000 Add history functionality for metric profiles

--- app/metricProfiles/model.go
+++ app/metricProfiles/model.go
@@ -26,9 +26,11 @@
 
 package metricProfiles
 
-// MongoInterface to retrieve and insert metricProfiles in mongo
-type MongoInterface struct {
+//MetricProfile struct used to retrieve and insert metricProfiles in mongo
+type MetricProfile struct {
 	ID       string    `bson:"id" json:"id"`
+	DateInt  int       `bson:"date_integer" json:"-"`
+	Date     string    `bson:"date" json:"date"`
 	Name     string    `bson:"name" json:"name"`
 	Services []Service `bson:"services" json:"services"`
 }
