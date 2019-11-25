82dee17653a40aa987de637a557f21f256664934
Konstantinos Kagkelidis <kaggis@gmail.com>
2019-11-25 14:27:03 +0200

    ARGO-2125 Fix regression of returning 404 when profile list were empty

--- app/metricProfiles/metricProfiles_test.go
+++ app/metricProfiles/metricProfiles_test.go
@@ -1014,6 +1014,41 @@ func (suite *MetricProfilesTestSuite) TestUpdate() {
 
 }
 
+func (suite *MetricProfilesTestSuite) TestListEmpty() {
+
+	session, err := mgo.Dial(suite.cfg.MongoDB.Host)
+	defer session.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	c := session.DB(suite.tenantDbConf.Db).C("metric_profiles")
+	c.DropCollection()
+
+	request, _ := http.NewRequest("GET", "/api/v2/metric_profiles", strings.NewReader(""))
+	request.Header.Set("x-api-key", suite.clientkey)
+	request.Header.Set("Accept", "application/json")
+	response := httptest.NewRecorder()
+
+	suite.router.ServeHTTP(response, request)
+
+	code := response.Code
+	output := response.Body.String()
+
+	emptyList := `{
+ "status": {
+  "message": "Success",
+  "code": "200"
+ },
+ "data": []
+}`
+	// Check that we must have a 200 ok code
+	suite.Equal(200, code, "Internal Server Error")
+	// Compare the expected and actual json response
+	suite.Equal(emptyList, output, "Response body mismatch")
+
+}
+
 func (suite *MetricProfilesTestSuite) TestDeleteNotFound() {
 
 	jsonInput := `{}`
