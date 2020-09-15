4b84ef9c14f61940a36bb4d8a5dc17f0fdad2fa2
Konstantinos Kagkelidis <kaggis@gmail.com>
2020-09-15 10:22:32 +0300

    ARGO-2474 Update user in tenant

--- app/tenants/routing.go
+++ app/tenants/routing.go
@@ -42,9 +42,11 @@ var appRoutesV2 = []respond.AppRoutes{
 	{"tenants.create", "POST", "/tenants", Create},
 	{"tenants.update_status", "PUT", "/tenants/{ID}/status", UpdateStatus},
 	{"tenants.create_user", "POST", "/tenants/{ID}/users", CreateUser},
+	{"tenants.update_user", "PUT", "/tenants/{ID}/users/{USER_ID}", UpdateUser},
 	{"tenants.update", "PUT", "/tenants/{ID}", Update},
 	{"tenants.delete", "DELETE", "/tenants/{ID}", Delete},
 	{"tenants.options", "OPTIONS", "/tenants", Options},
 	{"tenants.options", "OPTIONS", "/tenants/{ID}", Options},
 	{"tenants.options", "OPTIONS", "/tenants/{ID}/users", Options},
+	{"tenants.options", "OPTIONS", "/tenants/{ID}/users/{USER_ID}", Options},
 }
