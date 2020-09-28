ce69fbaab9729357333fde69505134e47ed84cd1
Konstantinos Kagkelidis <kaggis@gmail.com>
2020-09-28 14:15:23 +0300

    ARGO-2558 Show list of problematic endpoints

---
+++ app/issues/routing.go
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2015 GRNET S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ *
+ * The views and conclusions contained in the software and
+ * documentation are those of the authors and should not be
+ * interpreted as representing official policies, either expressed
+ * or implied, of GRNET S.A.
+ *
+ */
+
+package issues
+
+import (
+	"github.com/ARGOeu/argo-web-api/respond"
+	"github.com/gorilla/mux"
+)
+
+// HandleSubrouter contains the different paths to follow during subrouting
+func HandleSubrouter(s *mux.Router, confhandler *respond.ConfHandler) {
+
+	s = respond.PrepAppRoutes(s, confhandler, appRoutesV2)
+}
+
+var appRoutesV2 = []respond.AppRoutes{
+
+	{"issues.list_endpoints", "GET", "/{report_name}/endpoints", FlatListEndpointTimelines},
+	{"issues.options", "OPTIONS", "/{report_name}/endpoints", Options},
+}
