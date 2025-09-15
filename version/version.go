92571bf15e63e5204e16f4aa8491b86695886242
Konstantinos Kagkelidis <kaggis@gmail.com>
2025-09-15 16:35:33 +0300

    prep-v1.14.1

--- version/version.go
+++ version/version.go
@@ -6,7 +6,7 @@ import (
 
 var (
 	// Release version of the service. Bump it up during new version release
-	Release = "1.14.0"
+	Release = "1.14.1"
 	// Commit hash provided during build
 	Commit = "Unknown"
 	// BuildTime provided during build
